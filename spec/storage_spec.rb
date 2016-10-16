40533831ef84f965fd60bd6b79ca6e8a21d29308
CherevkoAndrew <cherevkoandrew@gmail.com>
2016-10-16 13:35:53 +0300

    storage implemented

--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -2,27 +2,21 @@ require_relative '../lib/storage'
 
 describe Storage do
 	s = Storage.new
-	it "should accept message and return key" do
-		key = s.push Message.new "message1", {:number_ofvisits => 1}
-	end
-	
-	it "should search message by key" do
-		expect(s.get(key).message).to be
+	key = s.push Message.new "message1", {:number_of_visits => 1}
+	key_t = s.push Message.new "message1", {:life_time => 0}
+	it "should accept message and return key to message" do
+		expect(s.get(key).read).to eq("message1")
 	end
 	
 	it "should destroy overdue messages" do
-
+		expect(s.get(key_t)).to eq(nil)	
 	end
 
 	it "should delete messages when \'number of visits\' condition fulfills" do
-		
+		expect(s.get(key)).to eq(nil)	
 	end
 
-	it "shouldn`t return messages with 0-lifetime" do
-		
-	end
-
-	it "shouldn`t return messages with 0-visits" do
-
+	it "should return nil for nonexistent messages" do
+		expect(s.get('nonexistentkey')).to eq(nil)	
 	end
 end 
