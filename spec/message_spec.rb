c5887b07c89b53c287394f7ffb1551db2bbd3d1c
CherevkoAndrew <cherevkoandrew@gmail.com>
2016-10-16 13:05:20 +0300

    Message class fixed

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,14 +2,27 @@ require_relative '../lib/message'
 
 describe Message do
 	s = 'Hello, kottans!!!'
-	m_time = Message.new s, {:life_time => 60*60}
-	m_visits = Message.new s, {:number_of_visits => 2}
-	m_both = Message.new s, {:life_time => 2*60*60, :number_of_visits => 1}
-	it "should have destruction option" do
-		expect(!!m_time.destruction_time).to eq(true)
-		expect(m_visits.number_of_visits).to eq(2)
-		expect(m_both.number_of_visits).to eq(1)
-		expect(!!m_both.destruction_time).to eq(true)
-		p m_time.destruction_time
+	m_time = Message.new '1:'+s, {:life_time => 60*60}
+	m_visits = Message.new '2:'+s, {:number_of_visits => 2}
+	m_both = Message.new '3:'+s, {:life_time => 2*60*60, :number_of_visits => 1}
+	m_t0 = Message.new '4'+s, {:life_time => 0}
+	m_v0 = Message.new '5'+s, {:number_of_visits => 0}
+
+	it "should accept destruction option" do
+		expect(m_t0).to_not be_alive
+		expect(m_v0).to_not be_alive
+		expect(m_time).to be_alive
+		expect(m_visits).to be_alive
+		expect(m_both).to be_alive
+	end
+	
+	it "should store message" do
+		expect(m_time.read).to eq('1:'+s)
+		expect(m_visits.read).to eq('2:'+s)
+	end
+
+	it "should use \'number of visits\' option" do
+		expect(m_visits.read).to eq('2:'+s)
+		expect(m_visits).to_not be_alive
 	end
 end
--- spec/storage_spec.rb
+++ spec/storage_spec.rb
@@ -1,12 +1,13 @@
 require_relative '../lib/storage'
 
 describe Storage do
+	s = Storage.new
 	it "should accept message and return key" do
-
+		key = s.push Message.new "message1", {:number_ofvisits => 1}
 	end
 	
 	it "should search message by key" do
-
+		expect(s.get(key).message).to be
 	end
 	
 	it "should destroy overdue messages" do
@@ -14,10 +15,14 @@ describe Storage do
 	end
 
 	it "should delete messages when \'number of visits\' condition fulfills" do
-
+		
 	end
 
-	it "" do
+	it "shouldn`t return messages with 0-lifetime" do
+		
+	end
+
+	it "shouldn`t return messages with 0-visits" do
 
 	end
 end 
